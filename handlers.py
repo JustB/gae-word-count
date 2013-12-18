75643b504d2917701ae015ba60b6c88fc0ac98e7
Giustino Borzacchiello <giustinob@gmail.com>
2013-12-18 12:34:10 +0100

    Added user authentication

--- handlers.py
+++ handlers.py
@@ -1,10 +1,23 @@
 import webapp2
 import jinja2
+from google.appengine.api import users
 
 
 class IndexHandler(webapp2.RequestHandler):
+    """
+    This is the main page. Here the user can upload new data and run MapReduce jobs.
+    """
     template_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'),
                                       autoescape=True)
 
     def get(self):
-        self.response.out.write(self.template_env.get_template('index.html').render())
+        user = users.get_current_user()
+        username = user.nickname()
+        logout_url = users.create_logout_url('/')
+
+        self.response.out.write(self.template_env.get_template('index.html').render(
+            {
+                'username': username,
+                'logout_url': logout_url
+            }
+        ))
