c62ed5b1e10ae2d43143c6d34823d68dfdb3ae07
Giustino Borzacchiello <giustinob@gmail.com>
2013-12-18 13:10:43 +0100

    Created Datastore entity for uploaded files

--- handlers.py
+++ handlers.py
@@ -4,6 +4,7 @@ import webapp2
 from google.appengine.api import users
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+from models import FileMetadata
 
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
@@ -11,6 +12,22 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     This handler upload data to the blobstore
     """
     def post(self):
+        upload_files = self.get_uploads('file')
+        blob_key = upload_files[0].key()
+        name = self.request.get('name')
+        user = users.get_current_user()
+        username = user.nickname()
+        date = datetime.datetime.now()
+        str_blob_key = str(blob_key)
+
+        key = FileMetadata.getKeyName(username, date, str_blob_key)
+        m = FileMetadata(key_name=key)
+        m.filename = name
+        m.uploadedOn = date
+        m.owner = user
+        m.blobkey = str_blob_key
+        m.put()
+
         self.redirect('/')
 
 
